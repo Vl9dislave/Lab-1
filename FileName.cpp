86152394b87fb5021c206c9abf30f3b7d3b07158
vladik <deez1dayz@gmail.com>
2026-03-25 19:36:38 +0200

    Add files via upload

--- FileName.cpp
+++ FileName.cpp
@@ -1,26 +1,44 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
-int main() {
 
-    using namespace std;
+using namespace std;
 
-    class FibonacciGenerator {
-    private:
-        long long a;
-        long long b;
+class FibonacciGenerator {
+private:
+    long long a;
+    long long b;
 
-    public:
-        FibonacciGenerator() : a(0), b(1) {}
-        long long next() {
-            long long current = a;
-            long long nextValue = a + b;
-            a = b;
-            b = nextValue;
-            return current;
-        }
-    };
+public:
+    FibonacciGenerator() : a(0), b(1) {}
 
+    long long next() {
+        long long current = a;
+        long long nextValue = a + b;
+        a = b;
+        b = nextValue;
+        return current;
+    }
+};
 
+void consumeWithTimeout(FibonacciGenerator& generator, int timeoutSeconds) {
+    auto startTime = chrono::steady_clock::now();
 
+    long long sum = 0;
+    int count = 0;
+
+    while (chrono::duration_cast<chrono::seconds>(
+        chrono::steady_clock::now() - startTime).count() < timeoutSeconds) {
+
+        long long value = generator.next();
+        count++;
+        sum += value;
+
+        double average = static_cast<double>(sum) / count;
+
+        cout << "Iteration: " << count
+            << " | Value: " << value
+            << " | Sum: " << sum
+            << " | Average: " << average << endl;
+    }
 }
